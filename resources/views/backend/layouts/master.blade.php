225a941d16685fa491d7e2fa8d21873f1abb7405
SakilAhmed007 <sakilhossain01969@gmail.com>
2021-03-12 21:12:35 +0600

    dashboard mastaring complete and artisan view pakage added

---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+  <meta name="author" content="Creative Tim">
+  <title>Argon Dashboard - Free Dashboard for Bootstrap 4</title>
+  <!-- Favicon -->
+  <link rel="icon" href="{{ asset('backend/assets/img/brand/favicon.png') }}" type="image/png">
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+  <!-- Icons -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
+  <link rel="stylesheet" href="{{ asset('backend/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
+  <!-- Page plugins -->
+  <!-- Argon CSS -->
+  <link rel="stylesheet" href="{{ asset('backend/assets/css/argon.css?v=1.2.0') }}" type="text/css">
+</head>
+
+<body>
+  <!-- Sidenav -->
+  @include('backend.layouts.side-nav')
+  <!-- Main content -->
+  <div class="main-content" id="panel">
+  <!-- Topnav -->
+  @include('backend.layouts.top-nav')
+  <!-- Header -->
+
+  <!-- main content -->
+  @yield('content')
+
+  <!-- Footer -->
+  <footer class="footer pt-0">
+      <div class="row align-items-center justify-content-lg-between">
+        <div class="col-lg-6">
+          <div class="copyright text-center  text-lg-left  text-muted">
+            &copy; 2021 <a href="" class="font-weight-bold ml-1" target="_blank"> 💖 Shakil Ahmed</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  </div>
+  <!-- Argon Scripts -->
+  <!-- Core -->
+  <script src="{{ asset('backend/assets/vendor/jquery/dist/jquery.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/vendor/js-cookie/js.cookie.js') }}"></script>
+  <script src="{{ asset('backend/assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+  <script src="{{ asset('backend/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
+
+  <!-- Argon JS -->
+  <script src="{{ asset('backend/assets/js/argon.js?v=1.2.0') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/backend/layouts/side-nav.blade.php
@@ -0,0 +1,35 @@
+<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-card-dark" id="sidenav-main">
+    <div class="scrollbar-inner">
+      <!-- Brand -->
+      <div class="sidenav-header  align-items-center">
+        <a class="navbar-brand" href="javascript:void(0)">
+          <img src="../assets/img/brand/blue.png" class="navbar-brand-img" alt="...">
+        </a>
+      </div>
+      <div class="navbar-inner">
+        <!-- Collapse -->
+        <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+          <!-- Nav items -->
+          <ul class="navbar-nav" >
+            <li class="nav-item">
+              <a class="nav-link active" href="dashboard.html">
+                <i class="ni ni-tv-2 text-primary"></i>
+                <span class="nav-link-text">Dashboard</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" onclick="event.preventDefault()" data-toggle="collapse" href="#collapseExample" role="button">
+                <i class="ni ni-planet text-orange"></i>
+                <span class="nav-link-text">Icons</span>
+              </a>
+              <ul class="collapse" id="collapseExample">
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-star-of-life"></i>hello</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-star-of-life"></i>hello</a></li>
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-star-of-life"></i>hello</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
